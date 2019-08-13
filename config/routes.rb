4d4a80fb32c4bcfb33f37d336e17da483dac5b72
cesar quirino <quirinocesar@gmail.com>
2019-08-13 16:07:37 +0200

    Task Backend Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+# Read All
+get '/tasks', to: 'tasks#index', as: :index
+
+# Read one
+
+get '/tasks/new', to: 'tasks#new', as: :new
+post '/tasks', to: 'tasks#create'
+
+get '/tasks/:id', to: 'tasks#show', as: :id
+
+# Create
+
+
+
+# Update
+get '/tasks/:id/edit', to: 'tasks#edit', as: :edit
+patch '/tasks/:id', to: 'tasks#update', as: :task
+
+# Delete
+delete '/tasks/:id', to: 'tasks#destroy', as: :destroy
+
 end
+
+
